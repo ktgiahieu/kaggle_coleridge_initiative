dcc83a0c8f53aaa0678c3f0e4dea25cc74454dc5
Hieu Khuong <ktgiahieu@gmail.com>
2021-06-20 21:45:54 +0700

    i

--- src/1st_level/distilbert_class_no_fold/infer.py
+++ src/1st_level/distilbert_class_no_fold/infer.py
@@ -70,4 +70,4 @@ def run():
 
 
 if __name__ == '__main__':
-	run()
+    run()
--- src/1st_level/distilbert_class_no_fold/train.py
+++ src/1st_level/distilbert_class_no_fold/train.py
@@ -90,4 +90,4 @@ def run():
 
 if __name__ == '__main__':
     utils.seed_everything(seed=config.SEED)
-	run()
+    run()
